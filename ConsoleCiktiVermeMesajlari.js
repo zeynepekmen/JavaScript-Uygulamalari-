c6cb7d9b08b024dc45b3110e77ad1399aa9c57be
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-07 21:11:53 +0300

    Create ConsoleCiktiVermeMesajlari.js

---
+++ ConsoleCiktiVermeMesajlari.js
@@ -0,0 +1,13 @@
+//console.log() - en temel mesaj verme yöntemidir.
+console.log("Çisem Yaşar"); //string
+console.log(55); //integer
+console.log(3.14); //float ya da double
+
+//console.error() - hata mesajıdır
+console.error("Bu bir hata mesajıdır");
+
+//console.warn() - uyarı mesajıdır
+console.warn("Bu bir uyarı mesajıdır");
+
+//console.info() - bilgi mesajıdır
+console.info("Bu bir bilgi mesajıdır");
