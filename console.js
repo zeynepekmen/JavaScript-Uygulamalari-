2fd9ef155e9297c797149ccc4a3a36303afd5cc8
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-10 16:39:06 +0300

    Add files via upload

---
+++ console.js
@@ -0,0 +1,30 @@
+console.log("zeynep"); //string
+console.log(15); //integer
+console.log(3.14); //float ya da double
+console.log(true); //boolean
+
+
+//window objesinin tüm özelliklerini görme
+console.dir(window);   
+
+//mevcut sayfanın alan adını görme
+console.dir(window.document.location.host);
+
+console.dir(window.document.documentURI);
+//documentURL yazdırma
+console.dir(window.location.port);
+
+
+
+
+
+
+
+
+//writeIn kullanımı
+document.writeIn("zeynep" + "<br>"); //string ifade
+document.writeIn(18 + "<br>"); //integer ifade
+document.writeIn(3.14 + "<br>"); //float ya da double ifade
+document.writeIn(true + "<br>"); //boolean ifade
+document.writeIn(null + "<br>"); //obje ifade
+
