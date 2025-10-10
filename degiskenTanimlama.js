861eee4a1f087d53d760823ab6c2e630026b89e4
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-10 16:39:43 +0300

    Add files via upload

---
+++ degiskenTanimlama.js
@@ -0,0 +1,37 @@
+//var ile değişken tanımlamak (Eski Yöntem)
+var ad="Zeynep";
+console.log(ad);
+
+
+//let ile değişkeni boş tanımlamak (undefined)
+let okul;
+console.log(okul);
+
+//let ile değişkene bilgi atamak
+okul="Gelişim Üniversitesi";
+console.log(okul);
+
+//let ile değişkene bilgi atayarak tanımlamak
+let program="Ön-Yüz Yazılım Geliştirme";
+console.log(program);
+
+//let ile tanımlanan değişken içindeki bilgiyi değiştirme
+program="Javascript Uygulamaları";
+console.log(program);
+
+//birleştirme ve ekleme işlemi 
+program= "Ders Bilgisi "+ program;
+console.log(program);
+
+
+
+
+
+//değişken ataması yapılmadan önce kullanmak
+//HATALI KULLANIM
+/*console.log(sifre);
+let sifre=1234;*/
+
+//DOĞRU KULLANIM
+let sifre=1234;
+console.log(sifre);  
