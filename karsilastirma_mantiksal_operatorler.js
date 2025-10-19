c4b7c12ddd9a4c6b5bbc9cc0b193046969206832
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-19 21:14:44 +0300

    Add basic eligibility check with logical operators

---
+++ karsilastirma_mantiksal_operatorler.js
@@ -0,0 +1,6 @@
+let yas = 28;
+let gelir = 35000;
+let krediNotu = 750;
+let temelKriterler = yas >= 20 && gelir >= 25000 && krediNotu >= 700;
+document.writeln(`Temel kriterlere uygun: ${temelKriterler}`+"<br>"); // true
+document.writeln("Temel kriterlere uygun");
