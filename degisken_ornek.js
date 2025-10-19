e16e23c9b61036bd4ed8744402b90296166b7e2f
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-19 21:14:23 +0300

    Add variable example and output summary

---
+++ degisken_ornek.js
@@ -0,0 +1,19 @@
+var ad = "EFE";
+console.log(ad); 
+let okul;
+console.log(okul); 
+okul = "Gökkuşağı Koleji";
+console.log(okul); 
+let program = "Sayısal";
+console.log(program); 
+program = "Sözel";
+console.log(program); 
+let sifre = "deneme";
+console.log(sifre);
+
+// çıktı Ekranı
+console.log("KAYIT ÖZETİ");
+console.log("Öğrenci: " + ad);
+console.log("Okul: " + okul);
+console.log("Program: " + program);
+console.log("Şifre: " + sifre);
