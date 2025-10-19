095c29a3385ca1b3194a7e3bbecf698d462f451f
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-19 21:13:18 +0300

    Create atama_operatorleri2.js

---
+++ atama_operatorleri2.js
@@ -0,0 +1,5 @@
+let bakiye=1000;
+bakiye+=500; //bakiye=bakiye+500
+bakiye-=200; //bakiye=bakiye-200
+bakiye*=1.1; //bakiye=bakiye*1,1
+document.writeln("Efe'nin son durumdaki maaşı: ",Math.round(bakiye));
