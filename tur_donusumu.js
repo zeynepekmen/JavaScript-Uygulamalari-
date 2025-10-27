367afe70d03d0358c3d155363e416106cd2b869f
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-10-27 22:19:52 +0300

    Add type conversion examples in tur_donusumu.js
    Implement type conversions between string, integer, and float.

---
+++ tur_donusumu.js
@@ -0,0 +1,16 @@
+// string ifadeyi integer ve floata çevirme
+let sayi="15";
+console.log(sayi," değeri ", typeof(sayi));
+//integera dönüştürme
+sayi1=parseInt(sayi);
+console.log(sayi1, " değeri ", typeof(sayi));
+
+//floata dönüştürme
+let sayi2=11.5;
+sayi2=parseFloat(sayi2);
+console.log(sayi2,"değeri",typeof(sayi2));
+
+//integerdan stringe dönüştürme
+let sayi3=55;
+sayi3=sayi3.toString();
+console.log(sayi3,"değeri",typeof(sayi3));
