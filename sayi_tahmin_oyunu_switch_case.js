a1bb672f133a44c491a1bfefa0880ce31d36639b
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-11-16 22:19:23 +0300

    Implement number guessing game using switch case

---
+++ sayi_tahmin_oyunu_switch_case.js
@@ -0,0 +1,30 @@
+//Sayı Tahmin Oyunu (Switch Case)
+let sayi=prompt("1 ile 5 arasında sayı giriniz");
+switch(sayi)
+{
+    case "1":
+        alert("Girilen Sayı 1'dir");
+        break;
+    case "2":
+        alert("Girilen Sayı 2'dir");
+        break;
+    case "3":
+        alert("Girilen Sayı 3'dür");
+        break;
+    case "4":
+        alert("Girilen Sayı 4'tür");
+        break;
+    case "5":
+        alert("Girilen Sayı 5'dir");
+        break;
+    default:
+        alert("Geçersiz sayı girişi yapılmıştır.");
+        break;
+}
+
+
+
+
+
+
+
