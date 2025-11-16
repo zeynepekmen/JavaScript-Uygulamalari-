b7d83eaa5045027b380bda0a38fb39a35ae473e9
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-11-16 22:13:01 +0300

    Add DOM manipulation examples in Dom_giris.js

---
+++ Dom_giris.js
@@ -0,0 +1,12 @@
+//tüm window objesine erişme
+console.log(window);
+//dokümanın lokasyon verisine erişme
+document.writeln(document.location+"<br>");
+//dokümanın pathname verisine erişme
+document.writeln(document.location.pathname+"<br>");
+//dokümanın body verisine erişme
+document.writeln(document.body);
+//body'nin arka planının rengini değiştirme
+document.body.style.backgroundColor="red";
+//body'de bulunan tüm yazıların rengini değiştirme
+document.body.style.color="white";
