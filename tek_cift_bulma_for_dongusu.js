fc3d549c4e27df025ac6a63aaa4488a44560110a
zeynep <fatma.ekmen@ogr.gelisim.edu.tr>
2025-11-16 22:17:34 +0300

    Add script to print even and odd numbers from 0 to 10

---
+++ tek_cift_bulma_for_dongusu.js
@@ -0,0 +1,13 @@
+//0'dan 10'a kadar çift sayıları yazdıralım (For Döngüsü)
+document.writeln("<u>0'dan 10'a kadar olan çift sayılar:</u> "+"<br>");
+for (let j=0; j<=10; j+=2)
+{
+    
+    document.writeln(j);
+}
+
+document.writeln("<br><u>1'den 10'a kadar olan tek sayılar:</u> "+"<br>");
+for (let i=1; i<=10;i+=2)
+{
+    document.writeln(i);
+}
